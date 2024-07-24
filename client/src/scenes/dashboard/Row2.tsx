7f52dcfd9d48fb4718b58a5449d4aae1e0f298ed
karnowsky1 <kestime9015@bths.edu>
2024-07-24 04:54:49 -0400

    set up api endpoint and types for Products on the front end

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -1,6 +1,10 @@
+import { useGetProductsQuery } from "../../state/api"
 import DashboardBox from "../../components/DashboardBox"
 
 const Row2 = (props: Props) => {
+  const { data } = useGetProductsQuery()
+  console.log("* - Row2 - data:", data)
+
   return (
     <>
       <DashboardBox gridArea="d"></DashboardBox>
