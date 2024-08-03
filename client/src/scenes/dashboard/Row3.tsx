055fa59387d56a634e26f294f30a403a06076da9
karnowsky1 <kestime9015@bths.edu>
2024-08-03 05:18:14 -0400

    completed the rest of the UI components for the initial dashboard displa

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -2,32 +2,37 @@ import { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery } from ".
 import BoxHeader from "../../components/BoxHeader"
 import DashboardBox from "../../components/DashboardBox"
 import FlexBetween from "../../components/FlexBetween"
-import { CartesianGrid, Cell, Line, LineChart, Pie, PieChart, ResponsiveContainer, Scatter, ScatterChart, Tooltip, XAxis, YAxis, ZAxis } from "recharts"
+import { Cell,Pie, PieChart } from "recharts"
 import { Box, Typography, useTheme } from "@mui/material"
 import { useMemo } from "react"
 import { DataGrid, GridCellParams } from "@mui/x-data-grid"
 
-const Row3 = (props: Props) => {
+const Row3 = () => {
   const { palette } = useTheme()
+  const pieColors = [palette.primary[800], palette.primary[500]]
   const { data: kpiData } = useGetKpisQuery();
   const { data: productData } = useGetProductsQuery();
-  const { data: transactionsData } = useGetTransactionsQuery();
+  const { data: transactionData } = useGetTransactionsQuery();
 
-  const styles ={ 
-    root: {
-      color: palette.grey[300],
-      border: "none"
-    },
-    cell: {
-      borderBottom: `1px solid ${palette.grey[800]} !important`
-    },
-    columnHeaders: {
-      borderBottom: `1px solid ${palette.grey[800]} !important`
-    },
-    columnSeparator: {
-      visibility: "hidden",
-    },
-  }
+  const pieChartData = useMemo(() => {
+    if (kpiData) {
+      const totalExpenses = kpiData[0].totalExpenses
+      return Object.entries(kpiData[0].expensesByCategory).map(
+        ([key, value]) => {
+          return [
+            {
+              name: key,
+              value: value,
+            },
+            {
+              name: `${key} of Total`,
+              value: totalExpenses - value,
+            }
+          ]
+        }
+      )
+    }
+  }, [kpiData])
 
   const productColumns = [
     {
@@ -46,6 +51,30 @@ const Row3 = (props: Props) => {
       headerName: "Price",
       flex: 0.5,
       renderCell: (params: GridCellParams) => `${params.value}`,
+    },
+  ]
+  const transactionColumns = [
+    {
+      field: "_id",
+      headerName: "id",
+      flex: 1,
+    },
+    {
+      field: "buyer",
+      headerName: "Buyer",
+      flex: 0.67,
+    },
+    {
+      field: "amount",
+      headerName: "Amount",
+      flex: 0.35,
+      renderCell: (params: GridCellParams) => `${params.value}`,
+    },
+    {
+      field: "productIds",
+      headerName: "Count",
+      flex: 0.1,
+      renderCell: (params: GridCellParams) => (params.value as Array<string>).length,
     },
   ]
   return (
@@ -91,12 +120,97 @@ const Row3 = (props: Props) => {
       </DashboardBox>
       <DashboardBox gridArea="h">
       <BoxHeader 
-          title="List of Products"
-          sideText={`${productData?.length} products`}
+          title="Recent Orders"
+          sideText={`${transactionData?.length} latest transactions`}
         />
+        <Box
+          mt="1rem"
+          p="0 0.5rem"
+          height="80%"
+          sx={{
+            "& .MuiDataGrid-root": {
+              color: palette.grey[300],
+              border: "none"
+              // can't target itself, so create a box around data grid and modify the class below it
+            },
+            "& .MuiDataGrid-cell": {
+              borderTop: `1px solid rgba(66, 66, 66, 0.5) !important`,
+              backgroundColor: `rgba(0, 0, 0, 0.1)`
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              borderTop: `1px solid rgba(66, 66, 66, 0.5) !important`,
+            },
+            "& .MuiDataGrid-columnSeparator": {
+              visibility: "hidden",
+            },
+            "& .css-1oudwrl::after": {
+              backgroundColor: "rgba(66, 66, 66, 0.5)"
+            }
+          }}
+        >
+          <DataGrid 
+            columnHeaderHeight={25}
+            rowHeight={35}
+            hideFooter={true}
+            rows={transactionData || []}
+            columns={transactionColumns}
+          />
+        </Box>
       </DashboardBox>
-      <DashboardBox gridArea="i"></DashboardBox>
-      <DashboardBox gridArea="j"></DashboardBox>
+      <DashboardBox gridArea="i">
+        <BoxHeader title="Expense Breakdown By Category" sideText="+4%" />
+        <FlexBetween mt="0.5rem" gap="0.5rem" p="0 1rem" textAlign="center">
+          {pieChartData?.map((data, index) =>(
+            <Box key={`${data[0].name}-${index}`}>
+              <PieChart 
+                width={110} 
+                height={100}
+              >
+                <Pie
+                  stroke="none"
+                  data={data}
+                  innerRadius={18}
+                  outerRadius={35}
+                  paddingAngle={2}
+                  dataKey="value"
+                >
+                  {data.map((entry, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={pieColors[index]}
+                    />
+                  ))}
+                </Pie>
+              </PieChart>
+              <Typography variant="h5">{data[0].name}</Typography>
+            </Box>
+          ))}
+        </FlexBetween>
+      </DashboardBox>
+      <DashboardBox gridArea="j">
+        <BoxHeader title="Overall Summary and Explanation Data" sideText="+4%" />
+        <Box
+          height="15px"
+          margin="1.25rem 1rem 0.4rem 1rem"
+          bgcolor={palette.primary[800]}
+          borderRadius="1rem"
+        >
+          <Box
+            height="15px"
+            bgcolor={palette.primary[600]}
+            borderRadius="1rem"
+            width="40%"
+          >
+          </Box>
+        </Box>
+        <Typography margin="0 1rem" variant="h6">
+        Effective financial analysis is crucial for understanding profit margins, 
+        evaluating product performance, and optimizing revenue streams. 
+        By examining financial statements and metrics, businesses can identify key 
+        trends and areas for improvement. Analyzing profit margins helps in assessing 
+        the profitability of individual products and services.
+        </Typography>
+      </DashboardBox>
     </>
   )
 } 
